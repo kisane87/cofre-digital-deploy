680c92b2fa9440b803b3e4c8bbca291cdaaedbcd
costela <guilhermeoliveira036789@gmail.com>
2026-05-11 11:45:59 -0300

    Personalizando o nome do cofre

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 ﻿from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def hello(): return 'Cofre Seguro!'
+def hello(): return '"Cofre do Guilherme: Acesso Restrito!"'
 if __name__ == '__main__': app.run()
