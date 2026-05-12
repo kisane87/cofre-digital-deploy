deebf89ff80c7959c50adc99c090ba1b9b8c609f
costela <guilhermeoliveira036789@gmail.com>
2026-05-12 11:33:18 -0300

    Projeto Finalizado

--- app/app.py
+++ app/app.py
@@ -1,5 +1,10 @@
-﻿from flask import Flask
+﻿from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
-def hello(): return '"Cofre do Guilherme: Acesso Restrito!"'
-if __name__ == '__main__': app.run()
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
